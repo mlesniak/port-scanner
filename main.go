163107442491f4b6dd2a57d0c363bf57b663eafe
Michael Lesniak <mail@mlesniak.com>
2018-02-23 06:33:10 +0100

    Add test code for initial connection

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import ("fmt")
+import (
+	"fmt"
+	"net"
+	"time"
+)
 
 func main() {
-    fmt.Println("Hello, Port Scan")
+	fmt.Println("Hello, Port Scan")
+	conn, err := net.DialTimeout("tcp", "localhost:631", time.Second * 5)
+	if err != nil {
+		fmt.Println("Connection timeout:", err)
+		return
+	}
+	fmt.Println("Closing connection.")
+	conn.Close()
 }
