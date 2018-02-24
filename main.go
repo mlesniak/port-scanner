05f9d8dd196a6c953f45c24f8a539e83b9837c44
Michael Lesniak <mail@mlesniak.com>
2018-02-24 16:07:06 +0100

    Add flag library for command line parsing

--- main.go
+++ main.go
@@ -1,15 +1,26 @@
 package main
 
 import (
+	"os"
 	"fmt"
 	"net"
 	"strconv"
 	"time"
+	"flag"
 )
 
+var hostname = flag.String("hostname", "localhost", "hostname")
+
 func main() {
-	for port := 0; port < 1024; port++ {
-		p := scanPort("tcp", "localhost", port, time.Second*5)
+	flag.Usage = func() {
+		fmt.Println("A simple port scanner in go.")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+	flag.Parse()
+	
+	for port := 70; port < 90; port++ {
+		p := scanPort("tcp", *hostname, port, time.Second*1)
 		if p {
 			fmt.Println(port, ":", p)
 		}
@@ -17,6 +28,7 @@ func main() {
 }
 
 func scanPort(tcpType, hostname string, port int, timeout time.Duration) bool {
+	fmt.Println("Trying", hostname + ":", port)
 	address := hostname + ":" + strconv.Itoa(port)
 	conn, err := net.DialTimeout(tcpType, address, timeout)
 	if err != nil {
