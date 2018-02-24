e4404fdb901ce94e1f0bbfbece3184b37af1a4b2
Michael Lesniak <mail@mlesniak.com>
2018-02-24 15:44:09 +0100

    Refactor into method

--- main.go
+++ main.go
@@ -3,16 +3,22 @@ package main
 import (
 	"fmt"
 	"net"
+	"strconv"
 	"time"
 )
 
 func main() {
-	fmt.Println("Hello, Port Scan")
-	conn, err := net.DialTimeout("tcp", "localhost:631", time.Second * 5)
+	p631 := scanPort("tcp", "localhost", 631, time.Second*5)
+	fmt.Println("631:", p631)
+}
+
+func scanPort(tcpType, hostname string, port int, timeout time.Duration) bool {
+	address := hostname + ":" + strconv.Itoa(port)
+	conn, err := net.DialTimeout(tcpType, address, timeout)
 	if err != nil {
-		fmt.Println("Connection timeout:", err)
-		return
+		return false
 	}
-	fmt.Println("Closing connection.")
+	// Should we use a defer here?
 	conn.Close()
+	return true
 }
