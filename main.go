fef796b7fdaf3612f2afe736f6fb27e826c05a2d
Michael Lesniak <mail@mlesniak.com>
2018-02-25 05:25:59 +0100

    Add mandatory check for argument

--- main.go
+++ main.go
@@ -9,21 +9,30 @@ import (
 	"flag"
 )
 
-var hostname = flag.String("hostname", "localhost", "hostname")
+var hostname = flag.String("hostname", "", "hostname of the target system")
 
 func main() {
-	flag.Usage = func() {
-		fmt.Println("A simple port scanner in go.")
-		flag.PrintDefaults()
-		os.Exit(1)
-	}
-	flag.Parse()
+	parseCommandLine()
 	
 	for port := 70; port < 90; port++ {
 		p := scanPort("tcp", *hostname, port, time.Second*1)
 		if p {
 			fmt.Println(port, ":", p)
 		}
+	}
+}
+
+func parseCommandLine() {
+	flag.Usage = func() {
+		fmt.Println("A simple port scanner in go.")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+	flag.Parse()
+
+	// Check that mandatory arguments are defined.
+	if (*hostname == "") {
+		flag.Usage()
 	}
 }
 
