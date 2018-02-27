a9e0939f3888222fb62fd748932602683c115a69
Michael Lesniak <mail@mlesniak.com>
2018-02-27 17:19:50 +0100

    Improve semaphore by adding an empty struct
    Thanks to /u/epiris on /r/golang for the hint.

--- semaphore.go
+++ semaphore.go
@@ -1,29 +1,19 @@
 // See http://www.golangpatterns.info/concurrency/semaphores
 package main
 
-
-// Empty is a dummy content.
-type Empty int
-
 // Semaphore is a semaphore for acquiring and releasing resources.
-type Semaphore chan Empty
-
+type Semaphore chan struct{}
 
 // Acquire n resources.
 func (s Semaphore) Acquire(n int) {
-    e := Empty(0)
-    for i := 0; i < n; i++ {
-        s <- e
-    }
+	for i := 0; i < n; i++ {
+		s <- struct{}{}
+	}
 }
 
 // Release n resources.
 func (s Semaphore) Release(n int) {
-    for i := 0; i < n; i++ {
-        <-s
-    }
+	for i := 0; i < n; i++ {
+		<-s
+	}
 }
-
-
-
-
